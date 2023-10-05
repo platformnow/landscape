2f09e9603a6d3a0f8942deae5c532a2a54158b4e
ThePlatformGuy <officialplatformguy@gmail.com>
2023-10-05 18:06:53 +0100

    Upgrade

--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -1,5 +1,6 @@
 import { CatalogBuilder } from '@backstage/plugin-catalog-backend';
-import { ScaffolderEntitiesProcessor } from '@backstage/plugin-scaffolder-backend';
+import { ScaffolderEntitiesProcessor } from '@backstage/plugin-catalog-backend-module-scaffolder-entity-model';
+import { UnprocessedEntitiesModule } from '@backstage/plugin-catalog-backend-module-unprocessed';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
 
@@ -9,6 +10,12 @@ export default async function createPlugin(
   const builder = await CatalogBuilder.create(env);
   builder.addProcessor(new ScaffolderEntitiesProcessor());
   const { processingEngine, router } = await builder.build();
+
+  const unprocessed = new UnprocessedEntitiesModule(
+      await env.database.getClient(),
+      router,
+  );
+  unprocessed.registerRoutes();
   await processingEngine.start();
   return router;
 }
