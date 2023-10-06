3ad0fcfd8a9ccfac86c77105248fd079fdd34d05
ThePlatformGuy <officialplatformguy@gmail.com>
2023-10-06 10:52:38 +0100

    Add the GithubMultiOrgEntityProvider to Pull in users and Teams

--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -3,12 +3,29 @@ import { ScaffolderEntitiesProcessor } from '@backstage/plugin-catalog-backend-m
 import { UnprocessedEntitiesModule } from '@backstage/plugin-catalog-backend-module-unprocessed';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
+import { GithubMultiOrgEntityProvider } from '@backstage/plugin-catalog-backend-module-github';
 
 export default async function createPlugin(
   env: PluginEnvironment,
 ): Promise<Router> {
   const builder = await CatalogBuilder.create(env);
   builder.addProcessor(new ScaffolderEntitiesProcessor());
+  // The GitHub URL below needs to match a configured integrations.github entry
+  // specified in your app-config.
+  builder.addEntityProvider(
+      GithubMultiOrgEntityProvider.fromConfig(env.config, {
+        id: 'production',
+        githubUrl: 'https://github.com',
+        // Set the following to list the GitHub orgs you wish to ingest from. You can
+        // also omit this option to ingest all orgs accessible by your GitHub integration
+        logger: env.logger,
+        schedule: env.scheduler.createScheduledTaskRunner({
+          frequency: { minutes: 60 },
+          timeout: { minutes: 15 },
+        }),
+      }),
+  );
+
   const { processingEngine, router } = await builder.build();
 
   const unprocessed = new UnprocessedEntitiesModule(
